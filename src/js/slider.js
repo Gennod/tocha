b88612f402048da56182e1859506dc477487081d
Ivan Blednov <i.blednov@thecoders.ru>
2021-10-26 16:49:10 +0500

    add telegram block without slider

---
+++ src/js/slider.js
@@ -0,0 +1,58 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const slidesWrapper = document.querySelector(".tariffs__slider-wrapper"),
+    slidesField = document.querySelector(".tariffs__slider-inner"),
+    width = window.getComputedStyle(slidesWrapper).width,
+    slides = document.querySelectorAll(".tariffs__slide"),
+    dotsWrapper = document.querySelector(".tariffs__dots"),
+    dotsArr = [];
+  
+  let slideIndex = 1,
+      offset = 0;
+
+  slidesField.style.width = 100 * slides.length + "%";
+  
+  slides.forEach(slide => {
+    slide.style.width = width;
+  });
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.classList.add("tariffs__dot");
+    
+    if (i == 0) {
+      dot.classList.add("tariffs__dot--active");  
+    }
+    dotsWrapper.append(dot);
+    dotsArr.push(dot);
+  }
+
+  dotsArr.forEach(dot => {
+    dot.addEventListener("click", (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      dotsArr.forEach(dot => {
+        if (dot.classList.contains("tariffs__dot--active")) {
+          dot.classList.remove("tariffs__dot--active");
+        }
+      });
+      dotsArr[slideIndex - 1].classList.add("tariffs__dot--active");
+
+    });
+  });
+  // dots.forEach(dot => {
+  //   dot.addEventListener("click", (e) => {
+  //     e.preventDefault();
+      
+
+  //     slidesField.style.transform = `translateX(-${offset}px)`;
+  //   })
+  // });
+
+});
