96f7545455ee6e1c99767152a722d218dfafb308
Ivan <oracll90@mail.ru>
2021-11-10 19:49:44 +0300

    fixing script file

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,62 +3,9 @@ var __webpack_exports__ = {};
 /*!**************************!*\
   !*** ./src/js/script.js ***!
   \**************************/
-const grid = document.createElement("div"),
-      slider = document.createElement("div"),
-      container = document.querySelectorAll(".container")[3],
-      burger = document.querySelector('.header__burger'),
+const burger = document.querySelector('.header__burger'),
       popup = document.querySelector('.header__popup'),
       cross = document.querySelector('.header__cross');
-grid.innerHTML = `
-  <div class="telegram__grid">
-  <div class="lines">
-      <img width="100%" src="./assets/icons/lines.svg" alt="lines">
-  </div>
-  <div class="telegram__column">
-      <div class="telegram__step">1</div>
-      <div class="telegram__subtitle">Telegram bot</div>
-      <div class="telegram__descr">Вы создаёте Telegram бота для вашего сайта</div>
-      <img width="233" height="471" class="telegram__img" src="./assets/img/iphoneX-1.png" alt="iphoneX-1">
-  </div>
-  <div class="telegram__column telegram__column--big">
-      <div class="telegram__step">2</div>
-      <div class="telegram__subtitle">Подключение Tocha</div>
-      <div class="telegram__descr">Tocha подключает Telegram бота к вашиму сайту</div>
-      <img width="233" height="471" class="telegram__img" src="./assets/img/iphoneX-2.png" alt="iphoneX-2">
-  </div>
-  <div class="telegram__column">
-      <div class="telegram__step">3</div>
-      <div class="telegram__subtitle">Telegram-чат</div>
-      <div class="telegram__descr">Консультант отвечает на вопросы посетителей сайта</div>
-      <img width="233" height="471" class="telegram__img" src="./assets/img/iphoneX-3.png" alt="iphoneX-3">   
-  </div>
-  </div>
-`;
-slider.innerHTML = `
-<div class="telegram__slider">
-<div class="telegram__slide">
-    <div class="telegram__step">1</div>
-    <h3 class="telegram__subtitle">Telegram bot</h3>
-    <p class="telegram__descr">Вы создаёте Telegram бота для вашего сайта</p>
-    <img width="233" height="471" class="telegram__img" src="./assets/img/iphoneX-1.png"
-        alt="iphoneX-1">
-</div>
-<div class="telegram__slide">
-    <div class="telegram__step">2</div>
-    <h3 class="telegram__subtitle">Подключение Tocha</h3>
-    <p class="telegram__descr">Tocha подключает Telegram бота к вашиму сайту</p>
-    <img width="233" height="471" class="telegram__img" src="./assets/img/iphoneX-2.png"
-        alt="iphoneX-2">
-</div>
-<div class="telegram__slide">
-    <div class="telegram__step">3</div>
-    <h3 class="telegram__subtitle">Telegram-чат</h3>
-    <p class="telegram__descr">Консультант отвечает на вопросы посетителей сайта</p>
-    <img width="233" height="471" class="telegram__img" src="./assets/img/iphoneX-3.png"
-        alt="iphoneX-3">
-</div>
-</div>
-`;
 burger.addEventListener('click', () => {
   popup.classList.add("visible");
   document.body.style.overflow = "hidden";
